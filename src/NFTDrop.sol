078255c0994ebc76f0bcdd332be658492c90cffb
Hoang Trinh <hoang.trinhj@gmail.com>
2023-03-29 22:50:16 +0700

    chore: update contract name

--- src/NFTDrop.sol
+++ src/NFTDrop.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Drop.sol";
 
-contract Contract is ERC721Drop {
+contract NFTDrop is ERC721Drop {
     constructor(
         string memory _name,
         string memory _symbol,
